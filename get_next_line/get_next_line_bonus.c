7426059fc555b8753eda510004ec65a9464fd7cd
Charlie ARMSTRONG <charmstr@e2r3p19.42.fr>
2019-11-22 03:11:51 +0100

    modified a if BUFFER_SIZE == 0 into BUFFER_SIZE <= 0 into the get_next_line_bonus.c the moulinette just validated my project before this commit

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: charmstr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 16:30:55 by charmstr          #+#    #+#             */
-/*   Updated: 2019/11/21 19:17:52 by charmstr         ###   ########.fr       */
+/*   Updated: 2019/11/22 03:10:28 by charmstr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	get_next_line(int fd, char **line)
 	t_fd			*fd_link;
 	int				result;
 
-	if (fd < 0 || BUFFER_SIZE == 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (-1);
 	if (!line)
 		if (!manage_link(fd, &head, REMOVE))
